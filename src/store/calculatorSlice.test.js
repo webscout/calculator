2ba503c558f91c1337866a2799f80bb16292bd95
alexeyb <avbaranov@detmir.ru>
2022-07-10 12:32:16 +0300

    Add calculator state with tests

--- src/store/calculatorSlice.test.js
+++ src/store/calculatorSlice.test.js
@@ -1,9 +1,180 @@
-import { calculatorSlice } from "./calculatorSlice";
+import {
+  calculatorSlice,
+  Operation,
+  selectDisplayValue,
+} from "./calculatorSlice";
+import { configureStore } from "@reduxjs/toolkit";
+
+const {
+  digitEntered,
+  decimalSeparatorEntered,
+  resultEntered,
+  operationEntered,
+  cleanEntered,
+} = calculatorSlice.actions;
+
+const createCalculatorStore = () =>
+  configureStore({
+    reducer: {
+      calculator: calculatorSlice.reducer,
+    },
+  });
+
+let calculatorStore = createCalculatorStore();
+
+const getCalculatorValue = () =>
+  selectDisplayValue(calculatorStore.getState().calculator);
+
+const dispatch = (action) => calculatorStore.dispatch(action);
 
 describe("Calculator tests", () => {
-  it("Initially value should be 0", () => {
-    const initialState = calculatorSlice.reducer(undefined, { type: "INIT" });
+  beforeEach(() => {
+    calculatorStore = createCalculatorStore();
+  });
 
-    expect(initialState.value).toBe(0);
+  it("Initially value should be 0", () => {
+    expect(getCalculatorValue()).toBe("0");
+  });
+
+  it("Digit stored after entered", () => {
+    calculatorStore.dispatch(
+      calculatorSlice.actions.digitEntered({ digit: "1" })
+    );
+
+    expect(getCalculatorValue()).toBe("1");
+  });
+
+  it("Digits added to each other", () => {
+    dispatch(digitEntered({ digit: "1" }));
+    dispatch(digitEntered({ digit: "2" }));
+    dispatch(digitEntered({ digit: "3" }));
+
+    expect(getCalculatorValue()).toBe("123");
+  });
+
+  it("Can not enter leading zeros", () => {
+    dispatch(digitEntered({ digit: "0" }));
+    dispatch(digitEntered({ digit: "0" }));
+    dispatch(digitEntered({ digit: "3" }));
+
+    expect(getCalculatorValue()).toBe("3");
+  });
+
+  it("Can enter decimal number", () => {
+    dispatch(digitEntered({ digit: "1" }));
+    dispatch(decimalSeparatorEntered());
+    dispatch(digitEntered({ digit: "3" }));
+
+    expect(getCalculatorValue()).toBe("1.3");
+  });
+
+  it("Can not enter two decimal separators", () => {
+    dispatch(digitEntered({ digit: "1" }));
+    dispatch(decimalSeparatorEntered());
+    dispatch(decimalSeparatorEntered());
+    dispatch(digitEntered({ digit: "3" }));
+
+    expect(getCalculatorValue()).toBe("1.3");
+  });
+
+  it("Enter 'result' multiple times without operation lead to the same result", () => {
+    dispatch(digitEntered({ digit: "1" }));
+    dispatch(digitEntered({ digit: "2" }));
+    dispatch(resultEntered());
+    dispatch(resultEntered());
+
+    expect(getCalculatorValue()).toBe("12");
+  });
+
+  it("2*3==12 (Enter 'result' after operation performs operation again)", () => {
+    dispatch(digitEntered({ digit: "2" }));
+    dispatch(operationEntered({ operation: Operation.Multiply }));
+    dispatch(digitEntered({ digit: "3" }));
+    dispatch(resultEntered());
+    dispatch(resultEntered());
+
+    expect(getCalculatorValue()).toBe("18");
+  });
+
+  describe("Basic math operations", () => {
+    it("2+3=4", () => {
+      dispatch(digitEntered({ digit: "2" }));
+      dispatch(operationEntered({ operation: Operation.Plus }));
+      dispatch(digitEntered({ digit: "3" }));
+      dispatch(resultEntered());
+
+      expect(getCalculatorValue()).toBe("5");
+    });
+
+    it("2-3=-1", () => {
+      dispatch(digitEntered({ digit: "2" }));
+      dispatch(operationEntered({ operation: Operation.Minus }));
+      dispatch(digitEntered({ digit: "3" }));
+      dispatch(resultEntered());
+
+      expect(getCalculatorValue()).toBe("-1");
+    });
+
+    it("3/2=1.5", () => {
+      dispatch(digitEntered({ digit: "3" }));
+      dispatch(operationEntered({ operation: Operation.Divide }));
+      dispatch(digitEntered({ digit: "2" }));
+      dispatch(resultEntered());
+
+      expect(getCalculatorValue()).toBe("1.5");
+    });
+
+    it("2*3=6", () => {
+      dispatch(digitEntered({ digit: "2" }));
+      dispatch(operationEntered({ operation: Operation.Multiply }));
+      dispatch(digitEntered({ digit: "3" }));
+      dispatch(resultEntered());
+
+      expect(getCalculatorValue()).toBe("6");
+    });
+  });
+
+  describe("Enter operation first (perform operation with initial zero)", () => {
+    it("-2=-2", () => {
+      dispatch(operationEntered({ operation: Operation.Minus }));
+      dispatch(digitEntered({ digit: "2" }));
+      dispatch(resultEntered());
+
+      expect(getCalculatorValue()).toBe("-2");
+    });
+
+    it("*2=0", () => {
+      dispatch(operationEntered({ operation: Operation.Multiply }));
+      dispatch(digitEntered({ digit: "2" }));
+      dispatch(resultEntered());
+
+      expect(getCalculatorValue()).toBe("0");
+    });
+  });
+
+  it("Clean screen after the operation doesn't affect next operations", () => {
+    dispatch(digitEntered({ digit: "2" }));
+    dispatch(operationEntered({ operation: Operation.Multiply }));
+    dispatch(digitEntered({ digit: "3" }));
+    dispatch(cleanEntered());
+
+    expect(getCalculatorValue()).toBe("0");
+
+    dispatch(digitEntered({ digit: "5" }));
+    dispatch(operationEntered({ operation: Operation.Plus }));
+    dispatch(digitEntered({ digit: "5" }));
+    dispatch(resultEntered());
+
+    expect(getCalculatorValue()).toBe("10");
+  });
+
+  it("2+/2=1 (Possible to override operation)", () => {
+    dispatch(digitEntered({ digit: "2" }));
+    dispatch(operationEntered({ operation: Operation.Plus }));
+    dispatch(operationEntered({ operation: Operation.Divide }));
+    dispatch(digitEntered({ digit: "2" }));
+    dispatch(resultEntered());
+
+    expect(getCalculatorValue()).toBe("1");
   });
 });
--- src/store/calculatorSlice.ts
+++ src/store/calculatorSlice.ts
@@ -1,15 +1,98 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export enum Operation {
+  Plus = "Plus",
+  Minus = "Minus",
+  Multiply = "Multiply",
+  Divide = "Divide",
+}
 
 export interface CalculatorState {
-  value: number;
+  value: string | null;
+  operation: Operation | null;
+  prevValue: string | null;
 }
 
 const initialState: CalculatorState = {
-  value: 0,
+  value: null,
+  operation: null,
+  prevValue: null,
+};
+
+const performOperation = (state: CalculatorState) => {
+  const { value, prevValue, operation } = state;
+
+  switch (operation) {
+    case Operation.Plus:
+      state.value = String(Number(prevValue) + Number(value));
+      break;
+    case Operation.Minus:
+      state.value = String(Number(prevValue) - Number(value));
+      break;
+    case Operation.Multiply:
+      state.value = String(Number(prevValue) * Number(value));
+      break;
+    case Operation.Divide:
+      state.value = String(Number(prevValue) / Number(value));
+      break;
+    default:
+      state.value = null;
+  }
+
+  state.prevValue = value;
+
+  return state;
 };
 
 export const calculatorSlice = createSlice({
-  name: "counter",
+  name: "calculator",
   initialState,
-  reducers: {},
+  reducers: {
+    digitEntered: (state, action: PayloadAction<{ digit: string }>) => {
+      const { digit } = action.payload;
+
+      if (!/^[0-9]$/.test(digit)) {
+        return;
+      }
+
+      if (state.value === "0" || state.value === null) {
+        state.value = digit;
+      } else {
+        state.value += digit;
+      }
+    },
+    decimalSeparatorEntered: (state) => {
+      if (!state.value.includes(".")) {
+        state.value += ".";
+      }
+    },
+    resultEntered: (state) => {
+      if (state.operation) {
+        performOperation(state);
+      }
+    },
+    cleanEntered: (state) => {
+      state.value = initialState.value;
+      state.prevValue = initialState.prevValue;
+      state.operation = initialState.operation;
+    },
+    operationEntered: (
+      state,
+      action: PayloadAction<{ operation: Operation }>
+    ) => {
+      if (state.value !== null) {
+        performOperation(state);
+      }
+
+      state.operation = action.payload.operation;
+    },
+  },
 });
+
+export const selectDisplayValue = (state: CalculatorState) => {
+  if (state.value !== null) {
+    return state.value;
+  }
+
+  return state.prevValue ?? "0";
+};
